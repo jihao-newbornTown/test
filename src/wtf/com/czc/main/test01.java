7b88edc2f0ef101a6cabf075a67303d4dd942be2
chenhao <chenhao@newborn-town.com>
2016-12-13 14:47:51 +0800

    main method

---
+++ src/wtf/com/czc/main/test01.java
@@ -0,0 +1,12 @@
+package wtf.com.czc.main; 
+/** 
+ * @author chenhao
+ * @version 创建时间：2016年12月13日 下午2:47:22 
+ */
+public class test01 {
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
